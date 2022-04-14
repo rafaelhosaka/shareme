78c6f5ebf9718bfdefec7086d43b36234e9792cd
rafaelhosaka <rafaelhosaka@gmail.com>
2022-04-14 16:39:15 -0300

    Add axios interceptor for errors

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,6 +2,19 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    console.log("Unexpected error");
+  }
+
+  return Promise.reject(error);
+});
+
 export default {
   get: axios.get,
   post: axios.post,
