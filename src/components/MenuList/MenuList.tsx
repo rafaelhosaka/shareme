a68ccb1052be00dcafa84200951a8b5644dd70f4
Rafael Hosaka <rafaelhosaka@gmail.com>
2022-10-03 15:38:11 -0300

    Fixed menu height problem

--- src/components/MenuList/MenuList.tsx
+++ src/components/MenuList/MenuList.tsx
@@ -1,5 +1,5 @@
 import css from "./Menu.module.scss";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 import { useToggle } from "../../hook/useToggle";
 
@@ -9,11 +9,36 @@ interface MenuListProps {
 }
 
 const MenuList = ({ title, children }: MenuListProps) => {
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 769px)" });
   const [expanded, toggle] = useToggle(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  function adjustSize() {
+    let parent = document.getElementsByClassName("left-content");
+
+    if (ref.current) {
+      if (!isMobile && parent[0]) {
+        ref.current.style.width = parent[0].clientWidth + "px";
+      }
+
+      if (isMobile && ref.current) {
+        ref.current.style.width = "100%";
+      }
+    }
+  }
+
+  useEffect(() => {
+    adjustSize();
+    window.addEventListener("resize", adjustSize);
+
+    return () => {
+      window.removeEventListener("resize", adjustSize);
+    };
+  }, [isMobile]);
 
   return (
     <div
+      ref={ref}
       className={
         isMobile && expanded
           ? `${css["menu__container"]} ${css["expanded"]}`
